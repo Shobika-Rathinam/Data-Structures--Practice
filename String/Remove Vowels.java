a9d7ca46c4daa5738a231feb1f6105b75e55ef66
Shobika-Rathinam <shobikar2501@gmail.com>
2025-01-30 17:08:46 +0530

    Update Remove Vowels.java

--- String/Remove Vowels.java
+++ String/Remove Vowels.java
@@ -1,1 +1,30 @@
+import java.util.*;
+public class RemoveVowels {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String S=sc.nextLine();
+        List<Character> L=new ArrayList<Character>();
+        L.add('a');
+        L.add('e');
+        L.add('i');
+        L.add('o');
+        L.add('u');
+        for(int i=0;i<S.length();i++)
+        {
+            char c=S.charAt(i);
+            if(L.contains(c))
+            {
+                continue;
+            }
+            else{
+                System.out.print(c);
+            }
+        }
+
+
+
+    }
+    
+}
 
